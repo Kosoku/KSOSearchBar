c5bf7e0526a15b4d30ac9dcc6ac682a0bb833bd3
William Towe <willbur1984@gmail.com>
2017-04-27 12:52:46 -0400

    Initial Commit

---
+++ KSOSearchBar/KSOSearchBar.h
@@ -0,0 +1,19 @@
+//
+//  KSOSearchBar.h
+//  KSOSearchBar
+//
+//  Created by William Towe on 4/27/17.
+//  Copyright © 2017 Kosoku Interactive, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSOSearchBar.
+FOUNDATION_EXPORT double KSOSearchBarVersionNumber;
+
+//! Project version string for KSOSearchBar.
+FOUNDATION_EXPORT const unsigned char KSOSearchBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSOSearchBar/PublicHeader.h>
+
+
